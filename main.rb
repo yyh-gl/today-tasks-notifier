8e24dd5c7c435de37648fce8b931c3c5735c059d
yyh-gl <yhonda.95.gl@gmail.com>
2019-02-09 14:07:26 +0900

    期限切れタスクも通知するように修正

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ end
 # ---------------------------------------------------------------
 # ライブラリ
 
-# 期限が本日中のタスクとWIPリストにあるタスクを取得
+# 期限が本日中または期限切れのタスクとWIPリストにあるタスクを取得
 def get_today_tasks(lists)
   # 今日の開始時間と終了時間を取得
   # タイムゾーンは関係ない。Timeクラスが吸収してくれる。
@@ -22,26 +22,37 @@ def get_today_tasks(lists)
   today_end_time = Time.local(now.year, now.month, now.day, 23, 59, 59)
 
   today_task_cards = []
+  limit_over_cards = []
   lists.each do |list|
     next if list.name == 'DONE'
 
     if list.name == 'WIP'
       # TODO: もっとスマートにできるはず
       list.cards.each do |card|
-        today_task_cards << card
+        if card.due.present? && card.due < today_start_time
+          limit_over_cards << card
+        else
+          today_task_cards << card
+        end
       end
     else
       list.cards.each do |card|
         next if card.due.nil?
 
-        today_task_cards << card if today_start_time <= card.due && card.due <= today_end_time
+        if today_start_time <= card.due && card.due <= today_end_time
+          today_task_cards << card
+        elsif card.due < today_start_time
+          limit_over_cards << card
+        end
       end
     end
   end
-  today_task_cards
+
+  return today_task_cards, limit_over_cards
 end
 
 # Slack通知
+# TODO: main と tech の決め打ち部分を可変にして汎用的にする
 def send_slack(today_tasks)
   slack_notifier = Slack::Notifier.new(ENV['WEBHOOK_URL'])
 
@@ -51,9 +62,11 @@ def send_slack(today_tasks)
 :mega:全部で #{today_tasks[:main].size + today_tasks[:tech].size} 個のタスクがあるよ:mega:
 
 
-            :mario2::dash: メイン :mario2::dash:
+           :mario2::dash: :kana-me::kana-i::kana-nn: :mario2::dash:
+
 MESSAGE
 
+  # メインボード内のタスク一覧
   today_tasks[:main].each_with_index do |task, i|
     message += ":small_orange_diamond:【M#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
 
@@ -63,7 +76,19 @@ MESSAGE
     message += "    :alarm_clock: `#{limit}`\n\n"
   end
 
-  message += "\n            :mario2::dash: 技術向上 :mario2::dash:\n"
+  # メインボード内の期限が切れているタスク一覧
+  if today_tasks[:main_limit].present?
+    today_tasks[:main_limit].each_with_index do |task, i|
+      message += ":space_invader:【L#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+
+      message += "    :curly_loop: _#{task.short_url} _\n"
+
+      message += "    :alarm_clock: `#{(task.due + 9.hour).to_s[0..-5]}` :face_palm:\n\n"
+    end
+  end
+
+  # 技術向上ボード内のタスク一覧
+  message += "\n          :mario2::dash: :kanji-waza::kanji-jutsu::kanji-mukau::kanji-ue: :mario2::dash:\n\n"
   today_tasks[:tech].each_with_index do |task, i|
     message += ":small_orange_diamond:【T#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
 
@@ -71,6 +96,17 @@ MESSAGE
 
     task.due.nil? ? limit = "なるはや" : limit = (task.due + 9.hour).to_s[0..-5]
     message += "    :alarm_clock: `#{limit}`\n\n"
+  end
+
+  # 技術向上ボード内の期限が切れているタスク一覧
+  if today_tasks[:tech_limit].present?
+    today_tasks[:tech_limit].each_with_index do |task, i|
+      message += ":space_invader:【L#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+
+      message += "    :curly_loop: _#{task.short_url} _\n"
+
+      message += "    :alarm_clock: `#{(task.due + 9.hour).to_s[0..-5]}` :face_palm:\n\n"
+    end
   end
 
   message += "\n            :mario2::dash: がんばろ :mario2::dash:\n"
@@ -90,9 +126,14 @@ main_lists = main_board.lists
 tech_lists = tech_board.lists
 
 # リストから今日やるべきタスクを取得
+main_today_tasks, main_limit_over_tasks = get_today_tasks(main_lists)
+tech_today_tasks, tech_limit_over_tasks = get_today_tasks(tech_lists)
+
 today_tasks = {
-  main: get_today_tasks(main_lists),
-  tech: get_today_tasks(tech_lists)
+  main: main_today_tasks,
+  main_limit: main_limit_over_tasks,
+  tech: tech_today_tasks,
+  tech_limit: tech_limit_over_tasks,
 }
 
 send_slack(today_tasks)
