4b3b4313b2e7025db6833c3b721041431ad67b18
yyh-gl <yhonda.95.gl@gmail.com>
2019-02-10 13:28:01 +0900

    期限が今日中のタスクのマークを変更

--- main.rb
+++ main.rb
@@ -3,24 +3,26 @@ require 'trello'
 require 'slack-notifier'
 
 # ---------------------------------------------------------------
-# Trello 初期設定
+## 初期設定
 
+## Trello
 Trello.configure do |config|
   config.developer_public_key = ENV['API_KEY']
   config.member_token = ENV['TOKEN']
 end
 
+## システム全般
+# 今日の開始時間と終了時間を取得
+# タイムゾーンは関係ない。Timeクラスが吸収してくれる。
+now = Time.now
+TODAY_START_TIME = Time.local(now.year, now.month, now.day, 0, 0, 0)
+TODAY_END_TIME = Time.local(now.year, now.month, now.day, 23, 59, 59)
+
 # ---------------------------------------------------------------
-# ライブラリ
+## ライブラリ
 
 # 期限が本日中または期限切れのタスクとWIPリストにあるタスクを取得
 def get_today_tasks(lists)
-  # 今日の開始時間と終了時間を取得
-  # タイムゾーンは関係ない。Timeクラスが吸収してくれる。
-  now = Time.now
-  today_start_time = Time.local(now.year, now.month, now.day, 0, 0, 0)
-  today_end_time = Time.local(now.year, now.month, now.day, 23, 59, 59)
-
   today_task_cards = []
   limit_over_cards = []
   lists.each do |list|
@@ -29,7 +31,7 @@ def get_today_tasks(lists)
     if list.name == 'WIP'
       # TODO: もっとスマートにできるはず
       list.cards.each do |card|
-        if card.due.present? && card.due < today_start_time
+        if card.due.present? && card.due < TODAY_START_TIME
           limit_over_cards << card
         else
           today_task_cards << card
@@ -39,9 +41,9 @@ def get_today_tasks(lists)
       list.cards.each do |card|
         next if card.due.nil?
 
-        if today_start_time <= card.due && card.due <= today_end_time
+        if TODAY_START_TIME <= card.due && card.due <= TODAY_END_TIME
           today_task_cards << card
-        elsif card.due < today_start_time
+        elsif card.due < TODAY_START_TIME
           limit_over_cards << card
         end
       end
@@ -71,7 +73,11 @@ MESSAGE
 
   # メインボード内のタスク一覧
   today_tasks[:main].each_with_index do |task, i|
-    message += ":small_orange_diamond:【M#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+    if task.due.present? && TODAY_START_TIME <= task.due && task.due <= TODAY_END_TIME
+      message += ":cubimal_chick:【M#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+    else
+      message += ":small_orange_diamond:【M#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+    end
 
     message += "    :curly_loop: _#{task.short_url} _\n"
 
@@ -95,7 +101,12 @@ MESSAGE
   # 技術向上ボード内のタスク一覧
   message += "\n          :mario2::dash: :kanji-waza::kanji-jutsu::kanji-mukau::kanji-ue: :mario2::dash:\n\n"
   today_tasks[:tech].each_with_index do |task, i|
-    message += ":small_orange_diamond:【T#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+    if task.due.present? && TODAY_START_TIME <= task.due && task.due <= TODAY_END_TIME
+      message += ":cubimal_chick:【T#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+    else
+      message += ":small_orange_diamond:【T#{format('%02d', i + 1)}】 *_#{task.name}_*\n"
+    end
+
     message += "    :curly_loop: _#{task.short_url} _\n"
 
     task.due.nil? ? limit = "なるはや" : limit = (task.due + 9.hour).to_s[0..-5]
@@ -121,7 +132,7 @@ MESSAGE
 end
 
 # ---------------------------------------------------------------
-# メイン処理
+## メイン処理
 
 # 指定ボードを取得
 main_board = Trello::Board.find(ENV['MAIN_BOARD_ID'])
